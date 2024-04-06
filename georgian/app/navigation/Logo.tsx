4dd2df655420619897aa474028540b0290e7109a
Rodion Chachura <geekrodion@gmail.com>
2024-04-06 14:03:36 +0400

    Fix logo link in navigation

--- georgian/app/navigation/Logo.tsx
+++ georgian/app/navigation/Logo.tsx
@@ -1,10 +1,9 @@
 import { ProductLogo } from '../product/ProductLogo'
-import { Path } from './Path'
 import { Link } from '@georgian/languages-ui/components/Link'
 
 export const Logo = () => {
   return (
-    <Link href={Path.Home}>
+    <Link href={'/'}>
       <ProductLogo />
     </Link>
   )

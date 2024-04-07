00e92c1c83c9f038591c1c806d3ff3a52806080a
Rodion Chachura <geekrodion@gmail.com>
2024-04-07 12:51:48 +0400

    Refactor InterviewQuestionItem component to use VStack and InterviewQuestionRow for better organization and readability

--- georgian/app/interview/CuratedQuestions/InterviewQuestionItem.tsx
+++ georgian/app/interview/CuratedQuestions/InterviewQuestionItem.tsx
@@ -7,19 +7,27 @@ import { VolumeIcon } from '@lib/ui/icons/VolumeIcon'
 import styled from 'styled-components'
 import { IconWrapper } from '@lib/ui/icons/IconWrapper'
 import { ArrowRightIcon } from '@lib/ui/icons/ArrowRightIcon'
-import { Center } from '@lib/ui/layout/Center'
 import { getColor } from '@lib/ui/theme/getters'
-
-const Container = styled.div`
-  display: grid;
-  grid-template-columns: auto 1fr;
-  align-items: center;
-  gap: 8px;
-  line-height: 1.5;
-`
+import { VStack } from '@lib/ui/layout/Stack'
+import { sameDimensions } from '@lib/ui/css/sameDimensions'
 
 const AnswerIconContainer = styled(IconWrapper)`
   color: ${getColor('textShy')};
+  width: 100%;
+`
+
+const rowIdentifierSize = '36px'
+
+const InterviewQuestionRow = styled.div`
+  display: grid;
+  grid-template-columns: ${rowIdentifierSize} 1fr;
+  align-items: center;
+  gap: 4px;
+  line-height: 1.5;
+`
+
+const SoundButton = styled(IconButton)`
+  ${sameDimensions(rowIdentifierSize)}
 `
 
 export const InterviewQuestionItem = ({
@@ -28,42 +36,45 @@ export const InterviewQuestionItem = ({
   const questionTranslation = value.translation[value.question]
 
   return (
-    <Container>
-      {value.questionAudioUrl ? (
-        <IconButton
-          title="Play question"
-          kind="secondary"
-          onClick={() => {
-            attempt(() => {
-              const audio = new Audio(value.questionAudioUrl)
-              audio.play()
-            }, undefined)
-          }}
-          icon={<VolumeIcon />}
-        />
-      ) : (
-        <div />
-      )}
-      <Text color="contrast" weight="semibold">
-        {value.question}
-      </Text>
-      {questionTranslation && (
-        <>
-          <div />
-          <Text color="supporting">{questionTranslation}</Text>
-        </>
-      )}
-
+    <VStack gap={8}>
+      <VStack gap={4}>
+        <InterviewQuestionRow>
+          {value.questionAudioUrl ? (
+            <SoundButton
+              title="Play question"
+              kind="secondary"
+              onClick={() => {
+                attempt(() => {
+                  const audio = new Audio(value.questionAudioUrl)
+                  audio.play()
+                }, undefined)
+              }}
+              icon={<VolumeIcon />}
+            />
+          ) : (
+            <div />
+          )}
+          <Text color="contrast" weight="semibold">
+            {value.question}
+          </Text>
+        </InterviewQuestionRow>
+        <InterviewQuestionRow>
+          {questionTranslation && (
+            <>
+              <div />
+              <Text color="supporting">{questionTranslation}</Text>
+            </>
+          )}
+        </InterviewQuestionRow>
+      </VStack>
       {value.answer && (
-        <>
-          <Center>
-            <AnswerIconContainer>
-              <ArrowRightIcon />
-            </AnswerIconContainer>
-          </Center>
+        <InterviewQuestionRow>
+          <AnswerIconContainer>
+            <ArrowRightIcon />
+          </AnswerIconContainer>
           <Text color="shy">{value.answer}</Text>
-        </>
+        </InterviewQuestionRow>
       )}
-    </Container>
+    </VStack>
   )
 }
